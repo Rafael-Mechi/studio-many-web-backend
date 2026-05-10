8f44d97d1a9b016794c6c9c02674e0c62d502eed
Rafael-Mechi <rafael.doliveira@sptech.school>
2026-05-10 18:27:03 -0300

    removendo campo de foto_url de profissional

--- src/main/java/many/studio/web_backend/entity/Profissional.java
+++ src/main/java/many/studio/web_backend/entity/Profissional.java
@@ -13,9 +13,6 @@ public class Profissional {
     @Column(name = "nome")
     private String nome;
 
-    @Column(name = "foto_url")
-    private String fotoUrl;
-
     @OneToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
@@ -25,7 +22,6 @@ public class Profissional {
     public Profissional(Integer id, String nome, String fotoUrl, LocalDateTime criadoEm, Usuario usuario) {
         this.id = id;
         this.nome = nome;
-        this.fotoUrl = fotoUrl;
         this.usuario = usuario;
     }
 
@@ -33,8 +29,6 @@ public class Profissional {
     public void setId(Integer id) { this.id = id; }
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-    public String getFotoUrl() { return fotoUrl; }
-    public void setFotoUrl(String fotoUrl) { this.fotoUrl = fotoUrl; }
     public Usuario getUsuario() { return usuario; }
     public void setUsuario(Usuario usuario) { this.usuario = usuario; }
 }
