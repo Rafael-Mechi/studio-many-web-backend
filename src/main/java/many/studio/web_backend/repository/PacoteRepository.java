2f8aa7656f13bff49e7da2628e9b597c975f43a4
Dereck-Baksa <dereck.baksa@sptech.school>
2026-05-17 22:08:41 -0300

    Endpoint get service e get pacote por service id

--- src/main/java/many/studio/web_backend/repository/PacoteRepository.java
+++ src/main/java/many/studio/web_backend/repository/PacoteRepository.java
@@ -3,7 +3,10 @@ package many.studio.web_backend.repository;
 import many.studio.web_backend.entity.Pacote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PacoteRepository extends JpaRepository<Pacote, Long> {
 
+    List<Pacote> findByServicoId(Long id);
 
 }
