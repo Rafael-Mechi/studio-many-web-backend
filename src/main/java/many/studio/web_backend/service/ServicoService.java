82c8e3899824164c919ca2558fb2acc23f8a7c11
Rafael-Mechi <rafael.doliveira@sptech.school>
2026-08-21 20:09:44 -0300

    corrigindo listagem de servicos

--- src/main/java/many/studio/web_backend/service/ServicoService.java
+++ src/main/java/many/studio/web_backend/service/ServicoService.java
@@ -65,6 +65,7 @@ public class ServicoService {
             }
 
             servicoListarDto.setProfissionais(profissionaisPorServicoDto);
+            lista.add(servicoListarDto);
         }
 
         return lista;
