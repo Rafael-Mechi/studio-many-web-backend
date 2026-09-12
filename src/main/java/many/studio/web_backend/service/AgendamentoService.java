db8583bc3f890dafb0ee58649e08d3317ab8f881
Rafael-Mechi <rafael.doliveira@sptech.school>
2026-09-12 14:50:51 -0300

    adicinando status de cancelado

--- src/main/java/many/studio/web_backend/service/AgendamentoService.java
+++ src/main/java/many/studio/web_backend/service/AgendamentoService.java
@@ -86,6 +86,7 @@ public class AgendamentoService {
                 "solicitar cancelamento",
                 "solicitar reagendamento",
                 "em atendimento",
+                "cancelado",
                 "aguardando sinal"
         );
 
